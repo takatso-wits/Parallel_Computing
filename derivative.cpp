bd6041cbdcdc80dcbe0bc7bdc52b9d088dcc8dea
takatso-wits <569869@students.wits.ac.za>
2018-10-05 17:42:53 +0200

    Partial Derivatives calculated
    The only thing that's missing is Parallelization.
    Use Sections

--- derivative.cpp
+++ derivative.cpp
@@ -1,59 +1,51 @@
 #include <iostream>
 #include <cmath>
-#include<vector>
 using namespace std;
 
 int main(){
 
 	double f_x, f_y,h = 0.01;
 	double x0 = 0.0, y0 = 0.0;
-	double x1 = 2.0, y1 = 4.0;
-	
+	double x1 = 2.0, y1 = 10.0;
 
-	 f_x = (pow((x0 + h),2) * y0 - pow((x0),2) * y0)/h;
-	 f_y = (pow((x0),2) * (y0 + h) - pow((x0),2) * y0)/h;	
-	
-	int x = static_cast<int>((x1-x0)/h);
-	int y = static_cast<int>((y1-y0)/h);
-	
-	double point[x][y];
-	
-	//initialize the grid.
+	long int x = static_cast<int>((x1-x0)/h);
+	long int y = static_cast<int>((y1-y0)/h);
+
+	//This is where I will be storing the Partial derivatives of both x and y.
+	double p_x[x], p_y[y];
+
+	//Now  I want to draw the function f(x,y) = x^2*y
+	double px[x][y];
+	double py[y][x];
 	for(int i = 0; i < x; i++){
-		for(int j= 0; j < y; j++){
-			point[i][j] = 0;
-		}
-	}
-	
-	for(int i = 0; i < x; i++){
-		for(int j= 0; j < y; j++){
-			point[i][j] = pow(x0,2) * y0;
+		y0 = 0.0;
+		for(int j = 0; j < y; j++){
+			px[i][j] = pow(x0,2)*y0;
 			y0 += h;
-			cout << y0 << endl;
 		}
 		x0 += h;
 	}
-	
-	
-	//print the graph
-	for(int i = 0; i < x; i++){
-		for(int j= 0; j < y; j++){
-			 cout << point[i][j] << " ";
+
+	for(int i = 0; i < y; i++){
+		x0 = 0.0;
+		for(int j = 0; j < x; j++){
+			py[i][j] = pow(x0,2)*y0;
+			x0 += h;
+		}
+		y0 += h;
+	}
+
+	// Now we have the graph and the domain
+	for(int row = 0; row < x; row++){
+		for(int col = 0; col < y; col++){
+			cout << py[row][col] << " ";
 		}
 		cout << endl;
 	}
-	 
-	for(int i = 0; i < x; i++){
-		for(int j= 0; j < y; j++){
-			f_x = (pow((point[i][j] + h),2) * point[i][j] - pow((point[i][j]),2) * point[i][j])/h;
-	 		f_y = (pow((point[i][j]),2) * (point[i][j] + h) - pow((point[i][j]),2) * point[i][j])/h;
-			cout << f_x << endl;
-			cout << f_y << endl;
-		}
-	}
-	 
-	  
-	
+
+	// Now I am calculating the Partial derivatives for x and y
 
 
+
+	return 0;
 }
